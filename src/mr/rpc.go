e54a547400c5a8e1f575402212a1a4a0bd05fbad
Apple <apple@AppledeMacBook-Pro-2.local>
2020-02-01 21:00:50 +0800

    Initialize

---
+++ src/mr/rpc.go
@@ -0,0 +1,32 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+type Args struct {
+//	X int
+}
+
+type Reply struct {
+	file string
+}
+
+//
+/*
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+*/
+
+// Add your RPC definitions here.
+
